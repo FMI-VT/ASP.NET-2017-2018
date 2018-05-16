3baad340917d8b6fc5a329407894e6211a97bf21
Dimiter Nikov <arruor@gmail.com>
2018-05-16 11:21:37 +0300

    Update references

--- src/CourseProject.DB/Entities/BaseEntity.cs
+++ src/CourseProject.DB/Entities/BaseEntity.cs
@@ -1,18 +1,19 @@
 ﻿namespace CourseProject.DB.Entities
 {
+    using System;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
 
-    public class BaseEntity : IBaseEntity<int, System.DateTime>
+    public class BaseEntity : IBaseEntity<int, DateTime>
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Key]
         public int Id { get; set; }
 
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-        public System.DateTime CreatedTime { get; set; }
+        public DateTime CreatedTime { get; set; }
 
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-        public System.DateTime UpdatedTime { get; set; }
+        public DateTime UpdatedTime { get; set; }
     }
 }
